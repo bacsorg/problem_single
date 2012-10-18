a703bc0c4cf072146612576fe58bd2f710795b49
Aleksey Filippov <sarum9in@gmail.com>
2012-10-18 13:43:15 +0400

    CLI integration with generator.

--- src/bin/cli.cpp
+++ src/bin/cli.cpp
@@ -8,6 +8,9 @@
 #include "yandex/contest/SystemError.hpp"
 #include "yandex/contest/TypeInfo.hpp"
 
+#include "bacs/single/problem/driver.hpp"
+#include "bacs/single/problem/generator.hpp"
+
 #include <iostream>
 
 #include <boost/program_options.hpp>
@@ -19,7 +22,12 @@ int main(int argc, char *argv[])
     try
     {
         std::vector<std::string> problems;
+        std::string generator_type, problem_destination, problem_prefix;
+        boost::property_tree::ptree generator_config; // note: unitialized
         desc.add_options()
+            ("generator,g", po::value<std::string>(&generator_type)->required(), "generator type")
+            ("prefix,R", po::value<std::string>(&problem_prefix)->required(), "problems prefix")
+            ("destination,d", po::value<std::string>(&problem_destination)->required(), "problems destination")
             ("problem,p", po::value<std::vector<std::string>>(&problems)->composing(), "problems");
         po::positional_options_description pdesc;
         pdesc.add("problem", -1);
@@ -29,6 +37,24 @@ int main(int argc, char *argv[])
         for (const std::string &problem: problems)
         {
             STREAM_INFO << "Processing \"" << problem << "\" problem...";
+            using namespace bacs::single::problem;
+            driver_ptr drv = driver::instance(problem);
+            if (!drv)
+            {
+                STREAM_ERROR << "Unable to initialize driver for \"" << problem << "\" problem.";
+                return 1;
+            }
+            STREAM_DEBUG << drv->overview().DebugString();
+            generator_ptr gen = generator::instance(generator_type, drv, generator_config);
+            if (!gen)
+            {
+                STREAM_ERROR << "Unable to initialize generator of \"" << generator_type << "\" type.";
+                return 1;
+            }
+            generator::options opts;
+            opts.destination = problem_destination;
+            opts.root_package = problem_prefix; // TODO: should be fixed, this implementation is unreliable
+            gen->generate(opts);
         }
     }
     catch (po::error &e)
