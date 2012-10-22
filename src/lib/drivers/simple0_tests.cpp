4a1464d44e4b97aafba2792bc24e15fe97a7ef76
Aleksey Filippov <sarum9in@gmail.com>
2012-10-23 01:06:47 +0400

    drivers::simple0_tests draft.

--- src/lib/drivers/simple0_tests.cpp
+++ src/lib/drivers/simple0_tests.cpp
@@ -1,3 +1,5 @@
+#include "bunsan/config.hpp"
+
 #include "simple0_tests.hpp"
 
 #include "bunsan/pm/depends.hpp"
@@ -11,7 +13,7 @@ namespace bacs{namespace single{namespace problem{namespace drivers
         {
             boost::property_tree::ptree config;
             config.put("build.builder", "[simple0 tests generator]");
-            config.put("call.wrapper", "TODO");// TODO
+            config.put("call.wrapper", "test_id_iterate");
             return config;
         }();
 
@@ -24,7 +26,14 @@ namespace bacs{namespace single{namespace problem{namespace drivers
     {
         boost::filesystem::create_directories(destination);
         bunsan::pm::depends index;
-        // TODO
+        index.source.import.package.insert(std::make_pair(".", "bacs/system/driver/simple0/tests"));
+        index.source.self.insert(std::make_pair("share/tests", "tests"));
+        boost::filesystem::create_directory(destination / "tests");
+        {
+            const boost::filesystem::directory_iterator end;
+            for (boost::filesystem::directory_iterator i(m_location); i != end; ++i)
+                boost::filesystem::copy_file(i->path(), destination / "tests" / i->path().filename());
+        }
         index.save(destination / "index");
     }
 }}}}
