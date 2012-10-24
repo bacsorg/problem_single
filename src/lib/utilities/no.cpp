4db0ca935a1f5f817387e6d858897bcdb5811c7f
Aleksey Filippov <sarum9in@gmail.com>
2012-10-25 01:12:09 +0400

    utilities::no implementation.

---
+++ src/lib/utilities/no.cpp
@@ -0,0 +1,27 @@
+#include "bacs/single/problem/utility.hpp"
+
+namespace bacs{namespace single{namespace problem{namespace utilities
+{
+    class no: public utility
+    {
+    public:
+        explicit no(const boost::property_tree::ptree &config): utility(config) {}
+
+        bool make_package(const boost::filesystem::path &destination,
+                          const bunsan::pm::entry &package) override
+        {
+            return false;
+        }
+
+    private:
+        static bool factory_reg_hook;
+    };
+
+    bool no::factory_reg_hook = utility::register_new("no",
+        [](const boost::filesystem::path &/*location*/,
+           const boost::property_tree::ptree &config)
+        {
+            utility_ptr tmp(new no(config));
+            return tmp;
+        });
+}}}}
