4fdd8953fed50259e7f7e3e11925c4d3ea0dbde8
Aleksey Filippov <sarum9in@gmail.com>
2013-01-15 20:46:56 +0400

    generators::internal0::internal0() declaration bug fix: became explicit.

--- src/lib/generators/internal0.hpp
+++ src/lib/generators/internal0.hpp
@@ -8,7 +8,7 @@ namespace bacs{namespace single{namespace problem{namespace generators
     class internal0: public generator
     {
     public:
-        internal0(const boost::property_tree::ptree &config);
+        explicit internal0(const boost::property_tree::ptree &config);
 
         api::pb::problem::Problem generate(const options &options_) override;
 
