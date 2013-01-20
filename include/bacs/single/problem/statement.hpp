50dee62a11b6122b214599f5f4d39dcb0ec6c6e2
Aleksey Filippov <sarum9in@gmail.com>
2013-01-20 21:56:48 +0400

    statement and statement::version implementation.

---
+++ include/bacs/single/problem/statement.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "bacs/single/problem/buildable.hpp"
+
+#include "bacs/single/api/pb/problem.pb.h"
+
+#include "bunsan/factory_helper.hpp"
+#include "bunsan/forward_constructor.hpp"
+
+#include <unordered_set>
+
+namespace bacs{namespace single{namespace problem
+{
+    class statement: public buildable
+    {
+    public:
+        class version: public buildable
+        BUNSAN_FACTORY_BEGIN(version, const boost::filesystem::path &/*location*/,
+                                      const boost::property_tree::ptree &/*config*/)
+        public:
+            /// \note config_location.parent_path() is location
+            static version_ptr instance(const boost::filesystem::path &config_location);
+
+            BUNSAN_FORWARD_EXPLICIT_CONSTRUCTOR(version, buildable)
+
+        public:
+            /// \warning package name is relative to statement version package
+            virtual api::pb::problem::Statement::Version info() const=0;
+
+        BUNSAN_FACTORY_END(version)
+
+        typedef std::shared_ptr<statement> statement_ptr;
+
+    public:
+        static statement_ptr instance(const boost::filesystem::path &location);
+
+    private:
+        explicit statement(const boost::filesystem::path &location);
+
+    public:
+        bool make_package(const boost::filesystem::path &destination,
+                          const bunsan::pm::entry &package) const override;
+
+        const std::vector<version_ptr> &versions() const;
+
+        /// \warning package names are relative to statement package
+        const api::pb::problem::Statement &info() const;
+
+    private:
+        const boost::filesystem::path m_location;
+        std::vector<version_ptr> m_versions;
+        api::pb::problem::Statement m_info;
+    };
+    typedef statement::statement_ptr statement_ptr;
+}}}
---
+++ src/lib/statement.cpp
@@ -0,0 +1,81 @@
+#include "bacs/single/problem/statement.hpp"
+
+#include "bunsan/filesystem/operations.hpp"
+
+#include "bunsan/pm/index.hpp"
+
+#include <boost/property_tree/ini_parser.hpp>
+
+namespace bacs{namespace single{namespace problem
+{
+    BUNSAN_FACTORY_DEFINE(statement::version)
+
+    statement::version_ptr statement::version::instance(const boost::filesystem::path &config_location)
+    {
+        boost::property_tree::ptree config;
+        boost::property_tree::read_ini(config_location.string(), config);
+        return instance(config.get<std::string>("build.builder"), config_location.parent_path(), config);
+    }
+
+    namespace
+    {
+        const bunsan::pm::entry versions_subpackage = "versions";
+    }
+
+    statement_ptr statement::instance(const boost::filesystem::path &location)
+    {
+        const statement_ptr tmp(new statement(location));
+        for (boost::filesystem::directory_iterator i(location), end; i != end; ++i)
+        {
+            if (i->path().filename().extension() == ".ini" &&
+                boost::filesystem::is_regular_file(i->path()))
+            {
+                tmp->m_versions.push_back(version::instance(i->path()));
+                api::pb::problem::Statement::Version &info = *(tmp->m_info.add_versions()) = tmp->m_versions.back()->info();
+                const bunsan::pm::entry package = versions_subpackage / info.package();
+                info.set_package(package.name());
+            }
+        }
+        return tmp;
+    }
+
+    statement::statement(const boost::filesystem::path &location): m_location(location) {}
+
+    bool statement::make_package(const boost::filesystem::path &destination,
+                                 const bunsan::pm::entry &package) const
+    {
+        const bunsan::pm::entry &root_package = package;
+        bunsan::filesystem::reset_dir(destination);
+        // resources
+        {
+            const boost::filesystem::path package_root = destination / "resources";
+            const bunsan::pm::entry package = root_package / "resources";
+            boost::filesystem::create_directory(package_root);
+            bunsan::pm::index index;
+            index.source.self.insert(std::make_pair("src", "src"));
+            bunsan::filesystem::copy_tree(m_location, package_root / "src");
+            index.save(package_root / "index");
+        }
+        // versions
+        {
+            const boost::filesystem::path versions_path = destination / versions_subpackage.location();
+            boost::filesystem::create_directory(versions_path);
+            for (const version_ptr &v: m_versions)
+            {
+                const bunsan::pm::entry package = v->info().package();
+                (void) v->make_package(versions_path / package.location(), root_package / versions_subpackage / package);
+            }
+        }
+        return true;
+    }
+
+    const std::vector<statement::version_ptr> &statement::versions() const
+    {
+        return m_versions;
+    }
+
+    const api::pb::problem::Statement &statement::info() const
+    {
+        return m_info;
+    }
+}}}
