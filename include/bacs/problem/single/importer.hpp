66557f38ecbe3845d70cac33257c7a255cd3e50a
Aleksey Filippov <sarum9in@gmail.com>
2013-06-04 00:54:16 +0400

    importer implementation.

---
+++ include/bacs/problem/single/importer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "bacs/problem/single/generator.hpp"
+
+#include "bacs/problem/importer.hpp"
+
+namespace bacs{namespace problem{namespace single
+{
+    class importer: public bacs::problem::importer
+    {
+    public:
+        explicit importer(const boost::property_tree::ptree &config);
+
+        bacs::problem::Problem convert(const options &options_) override;
+
+    private:
+        const generator_ptr m_generator;
+
+    private:
+        static const bool factory_reg_hook;
+    };
+}}}
---
+++ src/importer.cpp
@@ -0,0 +1,30 @@
+#include "bacs/problem/single/importer.hpp"
+#include "bacs/problem/single/driver.hpp"
+
+namespace bacs{namespace problem{namespace single
+{
+    static const char problem_type[] = "bacs/problem/single";
+
+    const bool importer::factory_reg_hook = importer::register_new(problem_type,
+        [](const boost::property_tree::ptree &config)
+        {
+            const importer_ptr tmp(new importer(config));
+            return tmp;
+        });
+
+    importer::importer(const boost::property_tree::ptree &config):
+        m_generator(generator::instance(config.get<std::string>("type"), config.get_child("config"))) {}
+
+    bacs::problem::Problem importer::convert(const options &options_)
+    {
+        const generator::options goptions = {
+            .driver = driver::instance(options_.problem_dir),
+            .destination = options_.destination,
+            .root_package = options_.root_package,
+        };
+        bacs::problem::Problem problem_info = m_generator->generate(goptions);
+        problem_info.mutable_info()->mutable_system()->set_hash(options_.hash.data(), options_.hash.size());
+        problem_info.mutable_info()->mutable_system()->set_problem_type(problem_type);
+        return problem_info;
+    }
+}}}
