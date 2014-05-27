f6298b75bdcde6514353f9f407773d3e5fe45c53
Aleksey Filippov <sarum9in@gmail.com>
2014-05-27 15:32:09 +0400

    drivers::simple0::tests refactoring.

--- src/drivers/simple0/tests.hpp
+++ src/drivers/simple0/tests.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bacs/problem/single/detail/list_tests.hpp>
 #include <bacs/problem/single/error.hpp>
 #include <bacs/problem/single/tests.hpp>
 
@@ -20,5 +21,31 @@ namespace bacs{namespace problem{namespace single{namespace drivers{
         static tests_ptr instance(
             const boost::filesystem::path &location,
             const boost::property_tree::ptree &config);
+
+        explicit tests(
+            const boost::filesystem::path &location,
+            const boost::property_tree::ptree &config);
+
+        std::unordered_set<std::string> data_set() const override
+        {
+            return m_tests.data_set();
+        }
+
+        std::unordered_set<std::string> test_set() const override
+        {
+            return m_tests.test_set();
+        }
+
+        bool make_package(const boost::filesystem::path &destination,
+                          const bunsan::pm::entry &package) const
+        {
+            return m_tests.make_package(destination, package);
+        }
+
+    private:
+        detail::list_tests m_tests;
+
+    private:
+        static const bool factory_reg_hook;
     };
 }}}}}
